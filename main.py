3eb2771ba64d23606a4afbf0d57f37207c1cc7ee
LaboNapitupulu <labo.123450037@student.itera.ac.id>
2024-12-02 20:33:35 +0700

    main set

---
+++ main.py
@@ -0,0 +1,26 @@
+import tkinter as tk
+
+from features.calculator import show_calculator
+
+
+def main():
+    window = tk.Tk()
+    window.title("Toolkit Produktivitas")
+    window.geometry("600x600")  # Menyesuaikan ukuran jendela aplikasi
+
+    frame = tk.Frame(window)
+    frame.pack(fill="both", expand=True)
+
+    # Membuat tombol navigasi yang lebih besar
+    menu = tk.Menu(window)
+    window.config(menu=menu)
+
+    app_menu = tk.Menu(menu, tearoff=0)
+    menu.add_cascade(label="Fitur", menu=app_menu)
+
+    app_menu.add_command(label="Kalkulator", command=lambda: show_calculator(window, frame))  # Perbaikan di sini, hanya frame
+
+    window.mainloop()
+
+if __name__ == "__main__":
+    main()
