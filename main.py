10ee83e9af597fa75afde462b15c6f2c5d32efca
LaboNapitupulu <labo.123450037@student.itera.ac.id>
2024-12-02 23:52:07 +0700

    last

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from features.calculator import show_calculator
 from features.todolist import show_todolist
 from features.timer import show_timer
 from features.quotes import show_random_quote  
+from features.notes import show_notes
 
 def main():
     window = tk.Tk()
@@ -24,6 +25,7 @@ def main():
     app_menu.add_command(label="Daftar Tugas", command=lambda: show_todolist(window, frame))
     app_menu.add_command(label="Timer Belajar", command=lambda: show_timer(window, frame))
     app_menu.add_command(label="Quote Generator", command=lambda: show_random_quote(frame))
+    app_menu.add_command(label="Catatan", command=lambda: show_notes(window, frame))
 
     window.mainloop()
 
